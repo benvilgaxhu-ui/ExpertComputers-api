0288274291c6afd5df55ff7480205d6e4196aee5
Krishna <benvilgaxhu@gmail.com>
2026-04-20 22:30:22 +0530

    Add config file to repo

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_BASE_URL = window.location.hostname === "localhost" 
     ? "http://localhost:5000" 
-    : "https://expertcomputers-backend.onrender.com";
+    : "https://expertcomputers.onrender.com";
 
 export default API_BASE_URL;
